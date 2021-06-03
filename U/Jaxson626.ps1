6bb54df4ad27eef7160f7f2c584e6e9ebb622a5f
Ben Jordan <ben@poecoh.com>
2021-06-03 19:44:37 -0400

    Create Jaxson626.ps1
    https://www.reddit.com/r/PowerShell/comments/nrj7le/enable_numlock_for_all_users_at_startup/

---
+++ U/Jaxson626.ps1
@@ -0,0 +1,38 @@
+[CmdletBinding()]
+
+Param (
+
+    [Parameter(ValueFromPipelineByPropertyName)]
+    [System.String[]]
+    $ComputerName,
+
+    [Parameter()]
+    [System.String]
+    $SubKey = 'Control Panel\Keyboard',
+
+    [Parameter()]
+    [System.String]
+    $ValueName = 'InitialKeyboardIndicators',
+
+    [Parameter()]
+    [System.String]
+    $Value
+
+)
+
+Begin {
+
+    $Hive = [Microsoft.Win32.RegistryHive]::CurrentUser
+
+} Process {
+    
+    $Status = [Microsoft.Win32.RegistryKey]::OpenRemoteBaseKey($Hive,$ComputerName).OpenSubKey($SubKey).GetValue($ValueName)
+    If ($Status -Eq 0) {
+    
+        [Microsoft.Win32.RegistryKey]::OpenRemoteBaseKey($Hive,$ComputerName).OpenSubKey($SubKey,$True).SetValue($ValueName,$Value)
+    
+    }
+
+} End {
+
+}
