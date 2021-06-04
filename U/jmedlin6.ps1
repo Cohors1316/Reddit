7ac2c1a860c9e4e90a5036f4bf675ba24baa3e26
Ben Jordan <ben@poecoh.com>
2021-06-03 20:36:21 -0400

    Update jmedlin6.ps1

--- U/jmedlin6.ps1
+++ U/jmedlin6.ps1
@@ -1,15 +1,16 @@
 # https://www.reddit.com/r/PowerShell/comments/nrplxo/datagridview/
+# didn't write it, just fixing reddit's mess of the code.
 
-$CertGrid = New-Object system.Windows.Forms.DataGridView
+$CertGrid = [System.Windows.Forms.DataGridView]::New()
 $CertGrid.width = 1275
 $CertGrid.height = 450
-$CertGrid.location = New-Object System.Drawing.Point(20,20)
+$CertGrid.location = [System.Drawing.Point]::New(20,20)
 $CertGrid.BackColor = [System.Drawing.ColorTranslator]::FromHtml("#ffffff")
 $CertGrid.TabIndex = 1
 $CertGrid.add_CurrentCellDirtyStateChanged($CertGrid_CurrentCellDirtyStateChanged)
-$CertGrid.RowHeadersVisible = $TRUE
+$CertGrid.RowHeadersVisible = $True
 $CertGrid.defaultCellStyle.wrapMode = [System.Windows.Forms.DataGridViewTriState]::True
 $CertGrid.AutoSizeRowsMode = [System.Windows.Forms.DataGridViewAutoSizeRowsMode]::AllCells
 $CertGrid.AutoSizeColumnsMode = [System.Windows.Forms.DataGridViewAutoSizeColumnsMode]::AllCells
-$CertGrid.AllowUserToDeleteRows = $TRUE
-$CertGrid.AutoGenerateColumns = $TRUE
+$CertGrid.AllowUserToDeleteRows = $True
+$CertGrid.AutoGenerateColumns = $True
