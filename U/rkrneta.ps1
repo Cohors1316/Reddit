24a6eacfff26c08800dafd2b963e5aee2681455d
Ben Jordan <ben@poecoh.com>
2021-10-13 13:11:00 -0400

    Create rkrneta.ps1

---
+++ U/rkrneta.ps1
@@ -0,0 +1,19 @@
+Write-Host -Object "`r▓░░░░░░░░░" -NoNewLine
+Start-Sleep -Seconds 1
+Write-Host -Object "`r▓▓░░░░░░░░" -NoNewLine
+Start-Sleep -Seconds 1
+Write-Host -Object "`r▓▓▓░░░░░░░" -NoNewLine
+Start-Sleep -Seconds 1
+Write-Host -Object "`r▓▓▓▓░░░░░░" -NoNewLine
+Start-Sleep -Seconds 1
+Write-Host -Object "`r▓▓▓▓▓░░░░░" -NoNewLine
+Start-Sleep -Seconds 1
+Write-Host -Object "`r▓▓▓▓▓▓░░░░" -NoNewLine
+Start-Sleep -Seconds 1
+Write-Host -Object "`r▓▓▓▓▓▓▓░░░" -NoNewLine
+Start-Sleep -Seconds 1
+Write-Host -Object "`r▓▓▓▓▓▓▓▓░░" -NoNewLine
+Start-Sleep -Seconds 1
+Write-Host -Object "`r▓▓▓▓▓▓▓▓▓░" -NoNewLine
+Start-Sleep -Seconds 1
+Write-Host -Object "`r▓▓▓▓▓▓▓▓▓▓"
