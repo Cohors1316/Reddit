6259dba6898d8d1d77f673054260df12e7d3559c
Ben Jordan <ben@poecoh.com>
2021-11-05 22:38:31 -0400

    Create SemiEvilMonkey.ps1

---
+++ U/SemiEvilMonkey.ps1
@@ -0,0 +1,12 @@
+# https://www.reddit.com/r/PowerShell/comments/qnlk0j/getwindows_features_parsed_to_table/
+
+Get-WindowsFeature |
+Where-Object -FilterScript {$_.InstallState -Eq 'Installed'} |
+Select-Object -Property Name, DisplayName, Subfeatures |
+ForEach-Object -Process {
+    Write-Ouput -InputObject ([PSCustomObject]@{
+        DisplayName = $_.DisplayName
+        Name = $_.Name
+        Subfeatures = $_.Subfeatures -Join ','
+    })
+}
