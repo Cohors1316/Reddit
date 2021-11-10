f833b85ea2e5eb783ec3c2140eb2ba9cea8a7711
Ben Jordan <ben@poecoh.com>
2021-11-09 20:48:43 -0500

    Update twoscoopsofpig.ps1
    Noticed a mistake and found a way to make it a little easier to use.

--- U/twoscoopsofpig.ps1
+++ U/twoscoopsofpig.ps1
@@ -1,6 +1,9 @@
+[CmdletBinding()]
+Param ($Test)
 Set-StrictMode -Version 3.0
 $ErrorActionPreference = 'Stop'
 $Error.Clear()
+
 
 Trap [System.Management.Automation.CommandNotFoundException] {
     Switch ($_.Exception.CommandName) {
@@ -9,7 +12,7 @@ Trap [System.Management.Automation.CommandNotFoundException] {
         } 'Test-Function' {
             Write-Host -Object 'Function trapped'
         } Default {
-            Write-Host -Object "$($_.Exception.CommandName) Trapped"
+            Write-Host -Object "$_ Trapped"
         }
     }
     Continue
@@ -18,9 +21,13 @@ Trap [System.Management.Automation.CommandNotFoundException] {
 If ($PSVersionTable.PSVersion.Major -GE 7) {
     Function Test-Function {Param($String)$String}
 } Else {
-    Invoke-Expression -Command 'Workflow Test-Workflow {Param($String)$String}'
+    Invoke-Expression -Command @'
+        Workflow Test-Workflow {Param($String)$String}
+        Workflow Test-Workflow2 {Param($String)$String}
+'@
 }
 
 Test-Function -String 'This is a function'
 Test-Workflow -String 'This is a workflow'
+Test-Workflow2 -String 'This is a second workflow'
 Write-Host -Object 'Script ended without committing suicide'
