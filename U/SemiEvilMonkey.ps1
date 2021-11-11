60c1b922248c1c4c1927c1470ca0e3a46302569f
Ben Jordan <ben@poecoh.com>
2021-11-10 19:14:50 -0500

    Update SemiEvilMonkey.ps1
    Missing t

--- U/SemiEvilMonkey.ps1
+++ U/SemiEvilMonkey.ps1
@@ -4,7 +4,7 @@ Get-WindowsFeature |
 Where-Object -FilterScript {$_.InstallState -Eq 'Installed'} |
 Select-Object -Property Name, DisplayName, Subfeatures |
 ForEach-Object -Process {
-    Write-Ouput -InputObject ([PSCustomObject]@{
+    Write-Output -InputObject ([PSCustomObject]@{
         DisplayName = $_.DisplayName
         Name = $_.Name
         Subfeatures = $_.Subfeatures -Join ','
